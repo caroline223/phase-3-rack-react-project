6e2a1ad91567547275f2c4ad7b82946a39cdee05
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-08-26 18:35:13 -0400

    Adjusted the get, post, and delete requests within the application.rb file

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -4,30 +4,27 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/tasks/) && req.get?
-      tasks = Task.all
-      return [200, { 'Content-Type' => 'application/json' }, [ tasks.to_json ]]
+    if req.path.match(/books/) && req.get?
+      books = Book.all
+      return [200, { 'Content-Type' => 'application/json' }, [ books.to_json ]]
       #message: taking a message hash and converting it to json server
 
-    elsif req.path.match(/categories/) && req.get?
-      categories = Category.all
-      return [200, { 'Content-Type' => 'application/json' }, [ categories.to_json ]]
+    elsif req.path.match(/authors/) && req.get?
+      authors = Author.all
+      return [200, { 'Content-Type' => 'application/json' }, [ authors.to_json ]]
 
 
-    elsif req.path.match(/tasks/) && req.post?
+    elsif req.path.match(/books/) && req.post?
      data = JSON.parse  req.body.read #takes sthg that is json and parse it and turn it into ruby
-     task = Task.create(data)
-     return [200, { 'Content-Type' => 'application/json' }, [ task.to_json ]]
+     book = Book.create(data)
+     return [200, { 'Content-Type' => 'application/json' }, [ book.to_json ]]
 
 
     elsif req.delete?
-      id = req.path_info.split("/tasks/").last
-      task = Task.find_by(id)
-      task.delete
-
-      return [200, { 'Content-Type' => 'application/json' }, [ {message: "Task deleted"}.to_json ]]
-
-
+      id = req.path_info.split("/books/").last
+      book = Book.find_by(id)
+      book.delete
+      return [200, { 'Content-Type' => 'application/json' }, [ {message: "Book deleted"}.to_json ]]
 
     else
       resp.write "Path Not Found"
